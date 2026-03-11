5eb6fde505f79e144b53e8c254c6c61c6cac3673
amersb <amerhaji15@gmail.com>
2026-03-11 13:46:40 +0100

    default hartslag

--- src/main/java/Hartslag/Api/HartslagController.java
+++ src/main/java/Hartslag/Api/HartslagController.java
@@ -1,11 +1,27 @@
-package Hartslag.Api;
+package org.babymonitor.api;
 
+import org.babymonitor.model.DefaultHartslag;
+import org.babymonitor.service.HartslagService;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequestMapping("/api/baby-hartslag")
+@CrossOrigin
 public class HartslagController {
 
-    public void test(){
+    private final HartslagService service;
 
+    public HartslagController(HartslagService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public List<DefaultHartslag> haalHartslagOp() {
+        return service.haalAlleMetingen();
     }
 }

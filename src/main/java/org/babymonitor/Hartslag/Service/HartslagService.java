a21118cf1fd22793dc23ed350cc73edbaa57bba4
amersb <535640@student.fontys.nl>
2026-03-23 15:46:18 +0100

    Defualthartslag

--- src/main/java/org/babymonitor/Hartslag/Service/HartslagService.java
+++ src/main/java/org/babymonitor/Hartslag/Service/HartslagService.java
@@ -1,6 +1,6 @@
 package org.babymonitor.Hartslag.Service;
 
-import org.babymonitor.model.DefaultHartslag;
+import org.babymonitor.Hartslag.Model.Hartslag;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
@@ -13,28 +13,59 @@ import java.util.Random;
 @Service
 public class HartslagService {
 
-    private final List<DefaultHartslag> metingen = new ArrayList<>();
+    private final List<Hartslag> metingen = new ArrayList<>();
     private final Random random = new Random();
     private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
 
-    public List<DefaultHartslag> haalAlleMetingen() {
-        return metingen;
+    private final int beginHartslag = 140;
+    private int huidigeHartslag = 140;
+    private final int variabiliteit = 20;
+
+    public List<Hartslag> haalAlleMetingen() {
+        return new ArrayList<>(metingen);
+    }
+
+    private int berekenVerandering(int huidige, int minBpm, int maxBpm, int[] stappen) {
+
+        double positie = (huidige - minBpm) / (double) (maxBpm - minBpm);
+
+        double kansOmhoog = 0.85 - 0.70 * positie;
+
+        boolean omhoog = random.nextDouble() < kansOmhoog;
+
+        int grootte = stappen[random.nextInt(stappen.length)];
+
+        if (omhoog) {
+            return Math.abs(grootte);
+        } else {
+            return -Math.abs(grootte);
+        }
     }
 
     @Scheduled(fixedRate = 1000)
-    public void maakNieuweAutomatischeHartslag() {
-        int bpm = 90 + random.nextInt(90);
-        String tijd = LocalTime.now().format(formatter);
+    public void maakNieuweHartslag() {
 
-        String status;
-        if (bpm < 110) {
-            status = "LAAG";
-        } else if (bpm > 160) {
-            status = "HOOG";
-        } else {
-            status = "NORMAAL";
+        int[] stappen = {2, 4, 6};
+
+        int minBpm = beginHartslag - variabiliteit;
+        int maxBpm = beginHartslag + variabiliteit;
+
+        int verandering = berekenVerandering(huidigeHartslag, minBpm, maxBpm, stappen);
+
+        int nieuweHartslag = huidigeHartslag + verandering;
+
+        if (nieuweHartslag < minBpm) {
+            nieuweHartslag = minBpm;
         }
 
-        metingen.add(new DefaultHartslag(bpm, tijd, status));
+        if (nieuweHartslag > maxBpm) {
+            nieuweHartslag = maxBpm;
+        }
+
+        huidigeHartslag = nieuweHartslag;
+
+        String tijd = LocalTime.now().format(formatter);
+
+        metingen.add(new Hartslag(huidigeHartslag, tijd));
     }
 }

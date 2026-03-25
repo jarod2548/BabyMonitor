098a0b97888a59585dbf27e30c69e807e57a62ac
jarod2548 <jarodkonijnenberg@gmail.com>
2026-03-25 11:41:42 +0100

    groepCreatie nu via websockets

--- src/main/java/org/babymonitor/connection/websocket/WebSocketConfig.java
+++ src/main/java/org/babymonitor/connection/websocket/WebSocketConfig.java
@@ -15,7 +15,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
         config.enableSimpleBroker("/topic");
-        config.setApplicationDestinationPrefixes("/group");
+        config.setApplicationDestinationPrefixes("/app");
     }
 
     @Override

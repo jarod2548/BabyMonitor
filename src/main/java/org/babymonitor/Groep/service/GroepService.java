315895773dc6cbd442dfe7a7ea7aefdef7faf9ea
Emirhan <524640@student.fontys.nl>
2026-03-25 12:06:52 +0100

    instructeur toegevoegd (groepsweergave)

--- src/main/java/org/babymonitor/Groep/service/GroepService.java
+++ src/main/java/org/babymonitor/Groep/service/GroepService.java
@@ -1,14 +1,13 @@
 package org.babymonitor.Groep.service;
 
-import org.babymonitor.Groep.model.Groep;
-import org.springframework.stereotype.Service;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
+
+import org.babymonitor.Groep.model.Groep;
+import org.springframework.stereotype.Service;
 
 @Service
 public class GroepService {
@@ -21,8 +20,11 @@ public class GroepService {
 
     public Groep maakGroep(String naam) {
         String groepId = UUID.randomUUID().toString();
-        String groepNaam = (naam == null || naam.isBlank()) ? "Groep X ": naam;
-        Groep groep = new Groep(groepId, groepNaam);
+        String groepNaam = (naam == null || naam.isBlank()) ? "Groep X " : naam;
+
+        // 
+        Groep groep = new Groep(groepId, groepNaam, "Instructeur A");
+
         groepen.put(groepId, groep);
         return groep;
     }

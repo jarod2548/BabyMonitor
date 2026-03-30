381057f5892ef260d05a2e8d766a6a03cb633753
jarod2548 <jarodkonijnenberg@gmail.com>
2026-03-30 15:29:05 +0200

    Update AccountRepository.java

--- src/main/java/org/babymonitor/Account/Repository/AccountRepository.java
+++ src/main/java/org/babymonitor/Account/Repository/AccountRepository.java
@@ -4,7 +4,9 @@ import org.babymonitor.Account.Model.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AccountRepository extends JpaRepository<Account, Long> {
     Optional<Account> findByUsername(String username);
 }

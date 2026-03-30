790e78dc974219ea66df35a94b585185476d9aa5
jarod2548 <jarodkonijnenberg@gmail.com>
2026-03-30 14:00:03 +0200

    Database creatie + Table migration

---
+++ src/main/java/org/babymonitor/Account/Repository/AccountRepository.java
@@ -0,0 +1,9 @@
+package org.babymonitor.Account.Repository;
+
+import org.babymonitor.Account.Model.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AccountRepository extends JpaRepository<Account, Long> {
+}

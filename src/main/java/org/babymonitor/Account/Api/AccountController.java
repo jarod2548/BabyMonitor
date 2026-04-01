0b4192faed144133261270acfabb779aa7b1f129
jarod2548 <jarodkonijnenberg@gmail.com>
2026-04-01 14:50:56 +0200

    JWT Token

--- src/main/java/org/babymonitor/Account/Api/AccountController.java
+++ src/main/java/org/babymonitor/Account/Api/AccountController.java
@@ -1,45 +1,23 @@
 package org.babymonitor.Account.API;
 
 import org.babymonitor.Account.DTO.LoginDTO;
-import org.babymonitor.Account.Model.Account;
-import org.babymonitor.Account.Service.AccountService;
 import org.babymonitor.Account.Service.LoginService;
-import org.babymonitor.config.CookieService;
-import org.babymonitor.config.JWTService;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/account")
 public class AccountController {
 
-    private AccountService accountService;
     private final LoginService loginService;
-    private final JWTService jwtService;
-    private final CookieService cookieService;
 
-    public AccountController(LoginService loginService,
-                             AccountService AccountService,
-                             JWTService JWTService,
-                             CookieService CookieService)
+    public AccountController(LoginService loginService)
     {
         this.loginService = loginService;
-        this.accountService = AccountService;
-        this.jwtService = JWTService;
-        this.cookieService = CookieService;
     }
 
     @PostMapping("/login")
-    public ResponseEntity<String> login(@RequestBody LoginDTO loginDTO)
+    public String login(@RequestBody LoginDTO loginDTO)
     {
-        Account model = loginDTO.convert();
-        Account data = loginService.login(model);
-
-        String token = jwtService.generateToken(data);
-
-        return ResponseEntity.ok().header("Set-Cookie",
-                        cookieService.createJwtCookie(token)
-                                .toString())
-                .body("Login Succesful");
+        return loginService.login(loginDTO);
     }
 }

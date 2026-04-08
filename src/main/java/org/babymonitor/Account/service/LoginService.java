205ea4b113007f3277e192086fac470f1a164dab
amersb <535640@student.fontys.nl>
2026-04-08 10:31:48 +0200

    hashing bij inloggen

--- src/main/java/org/babymonitor/Account/service/LoginService.java
+++ src/main/java/org/babymonitor/Account/service/LoginService.java
@@ -1,6 +1,7 @@
 package org.babymonitor.Account.service;
 
-import org.babymonitor.Account.model.LoginDTO;
+import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.babymonitor.Account.model.Account;
 import org.babymonitor.Account.repository.AccountRepository;
 import org.springframework.stereotype.Service;
@@ -9,16 +10,18 @@ import org.springframework.stereotype.Service;
 public class LoginService {
 
     private final AccountRepository repository;
+    private final PasswordEncoder passwordEncoder;
 
     public LoginService(AccountRepository accountRepository) {
         this.repository = accountRepository;
+        this.passwordEncoder = new Argon2PasswordEncoder(16, 32, 1, 1 << 14, 3);
     }
 
     public Account login(Account model) {
         Account account = repository.findByUsername(model.getUsername())
                 .orElseThrow(() -> new RuntimeException("User not found"));
 
-        if (!account.getPassword().equals(model.getPassword())) {
+        if (!passwordEncoder.matches(model.getPassword(), account.getPassword())) {
             throw new RuntimeException("Invalid password");
         }
 

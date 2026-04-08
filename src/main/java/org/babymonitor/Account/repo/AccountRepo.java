124028d95c5fe234425ab063a30071f9129430ee
jarod2548 <jarodkonijnenberg@gmail.com>
2026-04-08 09:20:33 +0200

    Delete src/main/java/org/babymonitor/Account/repo directory

--- src/main/java/org/babymonitor/Account/repo/AccountRepo.java
+++
@@ -1,7 +0,0 @@
-package org.babymonitor.Account.repo;
-
-import org.babymonitor.Account.Model.Account;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AccountRepo extends JpaRepository<Account, Long> {
-}

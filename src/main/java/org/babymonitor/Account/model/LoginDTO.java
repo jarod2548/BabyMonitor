1f30be29f1405d8f82ebb03fa657a6fbdb1b4ca3
amersb <535640@student.fontys.nl>
2026-04-15 10:48:31 +0200

    Account Junit test

--- src/main/java/org/babymonitor/Account/model/LoginDTO.java
+++ src/main/java/org/babymonitor/Account/model/LoginDTO.java
@@ -1,7 +1,14 @@
 package org.babymonitor.Account.model;
+import jakarta.validation.constraints.*;
 
 public class LoginDTO {
+   @Size(min = 1, max = 255, message = "Email must be between 1 and 255 characters.")
+   @NotBlank(message = "Email cannot be blank")
+   @Email(message = "Please provide a valid email address.")
     private String email;
+
+   @NotBlank(message = "Password cannot be blank")
+   @Size(min = 8, max = 100, message = "Password must be between 8 and 100 characters.")
     private String password;
 
     public String getEmail() {
